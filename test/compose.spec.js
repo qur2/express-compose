dd383de5af4a91ccd1def252408e832aa424a19c
Aurélien Scoubeau <aurelien.scoubeau@gmail.com>
2012-09-22 19:46:54 +0200

    Add tested render method.
    Version bump.

--- test/compose.spec.js
+++ test/compose.spec.js
@@ -121,20 +121,128 @@ describe('compose', function() {
 		});
 	});
 
-	describe('res-bound resource', function() {
+	describe('surcharges render', function() {
 		beforeEach(function() {
 			app = express();
 			app.get('/');
 		});
 
+		it('with usual signature', function(done) {
+			var user = { name: 'tobi' };
+			var template = __dirname + '/fixtures/user.jade';
+			app.use(function(req, res) {
+				compose.render(res, template, user);
+			});
+
+			request(app)
+				.get('/')
+				.expect(200)
+				.end(function(err, res) {
+					if (err) throw err;
+					expect(res.text).to.equal('<p>tobi</p>');
+					done();
+				});
+		});
+
+		it('with multiple options', function(done) {
+			var users = [{ name: 'tobi' }, { name: 'paul' }, { name: 'aure' }];
+			var template = __dirname + '/fixtures/user.jade';
+			app.use(function(req, res) {
+				compose.render(res, template, users);
+			});
+
+			request(app)
+				.get('/')
+				.expect(200)
+				.end(function(err, res) {
+					if (err) throw err;
+					expect(res.text).to.equal('<p>tobi</p><p>paul</p><p>aure</p>');
+					done();
+				});
+		});
+
+		it('with multiple templates', function(done) {
+			var user = { name: 'tobi' };
+			var templates = [
+				__dirname + '/fixtures/user.jade',
+				__dirname + '/fixtures/deco-simple.jade'
+			];
+			app.use(function(req, res) {
+				compose.render(res, templates, user);
+			});
+
+			request(app)
+				.get('/')
+				.expect(200)
+				.end(function(err, res) {
+					if (err) throw err;
+					expect(res.text).to.equal('<p><p>tobi</p></p>');
+					done();
+				});
+		});
+
+		it('with multiple templates and options', function(done) {
+			var users = [{ name: 'tobi' }, { name: 'paul' }, { name: 'aure' }];
+			var templates = [
+				__dirname + '/fixtures/user.jade',
+				__dirname + '/fixtures/deco-simple.jade'
+			];
+			app.use(function(req, res) {
+				compose.render(res, templates, users);
+			});
+
+			request(app)
+				.get('/')
+				.expect(200)
+				.end(function(err, res) {
+					if (err) throw err;
+					expect(res.text).to.equal('<p><p>tobi</p><p>paul</p><p>aure</p></p>');
+					done();
+				});
+		});
+
+		it('with multiple templates and options and callback', function(done) {
+			var postRender = false;
+			var users = [{ name: 'tobi' }, { name: 'paul' }, { name: 'aure' }];
+			var templates = [
+				__dirname + '/fixtures/user.jade',
+				__dirname + '/fixtures/deco-simple.jade'
+			];
+			app.use(function(req, res) {
+				compose.render(res, templates, users, function(err, rendered) {
+					postRender = true;
+					res.send(rendered);
+				});
+			});
+
+			request(app)
+				.get('/')
+				.expect(200)
+				.end(function(err, res) {
+					if (err) throw err;
+					expect(res.text).to.equal('<p><p>tobi</p><p>paul</p><p>aure</p></p>');
+					expect(postRender).to.be.true;
+					done();
+				});
+		});
+	});
+
+	describe('wrapped resource', function() {
+		beforeEach(function() {
+			app = express();
+			app.get('/');
+		});
+
+
 		it('decorates', function(done) {
 			var user = { name: 'tobi' };
+			var templates = [
+				__dirname + '/fixtures/user.jade',
+				__dirname + '/fixtures/deco-simple.jade',
+				__dirname + '/fixtures/deco-simple.jade'
+			];
 			app.use(function(req, res) {
-				compose.wrap(res).decorate([
-					__dirname + '/fixtures/user.jade',
-					__dirname + '/fixtures/deco-simple.jade',
-					__dirname + '/fixtures/deco-simple.jade'
-				], user);
+				compose.wrap(res).decorate(templates, user);
 			});
 
 			request(app)
@@ -171,7 +279,7 @@ describe('compose', function() {
 				});
 		});
 
-		it('decorates with custom decoration key', function(done) {
+		it('decorates with wrapping options', function(done) {
 			var user = { name: 'tobi' };
 			app.use(function(req, res) {
 				compose.wrap(res, {compose: 'customKey'}).decorate([
@@ -222,6 +330,33 @@ describe('compose', function() {
 				.end(function(err, res) {
 					if (err) throw err;
 					expect(res.text).to.equal('<p>tobi</p><p>paul</p><p>aure</p>');
+					done();
+				});
+		});
+
+		it('renders with wrapping options', function(done) {
+			var postRender = false;
+			var users = [{ name: 'tobi' }, { name: 'paul' }, { name: 'aure' }];
+			var templates = [
+				__dirname + '/fixtures/user.jade',
+				__dirname + '/fixtures/deco-custom.jade'
+			];
+			app.use(function(req, res) {
+				compose.wrap(res, {
+					compose: 'customKey'
+				}).render(templates, users, function(err, rendered) {
+					postRender = true;
+					res.send(rendered);
+				});
+			});
+
+			request(app)
+				.get('/')
+				// .expect(200)
+				.end(function(err, res) {
+					if (err) throw err;
+					expect(res.text).to.equal('<p><p>tobi</p><p>paul</p><p>aure</p></p>');
+					expect(postRender).to.be.true;
 					done();
 				});
 		});
